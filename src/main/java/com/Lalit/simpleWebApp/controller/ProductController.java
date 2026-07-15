bf322c7dcce86de1abbea1dea63601596531181e
LalitShrma3012 <lalit8601sh@gmail.com>
2026-07-15 09:38:11 +0530

    Adding more controllers for Product

--- src/main/java/com/Lalit/simpleWebApp/controller/ProductController.java
+++ src/main/java/com/Lalit/simpleWebApp/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.Lalit.simpleWebApp.controller;
 import com.Lalit.simpleWebApp.model.Product;
 import com.Lalit.simpleWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +13,29 @@ public class ProductController {
     @Autowired
     ProductService prodService;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts(){
         return prodService.getProduct();
     }
+
+    @GetMapping("/products/{id}")
+    public Product getProductById(@PathVariable int id){
+        return prodService.getById(id);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product product){
+        prodService.addProduct(product);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product prod){
+        prodService.updateProduct(prod);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public void deleteProduct(@PathVariable int id){
+        prodService.deleteProduct(id);
+    }
+
 }
--- src/main/java/com/Lalit/simpleWebApp/service/ProductService.java
+++ src/main/java/com/Lalit/simpleWebApp/service/ProductService.java
@@ -3,19 +3,50 @@ package com.Lalit.simpleWebApp.service;
 import com.Lalit.simpleWebApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101, "IPhone", 50000),
             new Product(102, "Canon Camera", 600000),
-            new Product(103, "Asus VivoBook", 120000)
+            new Product(103, "Asus VivoBook", 120000))
     );
 
     public List<Product> getProduct(){
         return products;
+    }
+
+    public Product getById(int id) {
+        return products.stream()
+                .filter(p -> p.getProdId()==id)
+                .findFirst().orElse(new Product(100, "No Item", 0));
+    }
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public void updateProduct(Product prod) {
+        for(int i=0; i<products.size(); i++){
+            if(products.get(i).getProdId() == prod.getProdId()){
+                products.set(i, prod);
+                return;
+            }
+        }
+    }
+
+    public void deleteProduct(int id) {
+        int idx = 0;
+        for(int i=0; i<products.size(); i++){
+            if(products.get(i).getProdId() == id){
+                idx = i;
+                break;
+            }
+        }
+        products.remove(products.get(idx));
     }
 }
